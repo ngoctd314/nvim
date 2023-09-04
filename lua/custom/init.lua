86f37fddbde0606eca89f1a4f3aea390baeca488
ngoctd <tranngoc.contact@gmail.com>
2023-09-04 19:15:33 +0700

    u

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,7 +1,22 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+--
+-- vim.cmd [[
+--   au BufWinLeave * silent mkview
+--   au BufWinEnter * silent loadview
+-- ]]
+
+autocmd("BufWinLeave", {
+  pattern = "*",
+  command = "silent! mkview",
+})
+
+autocmd("BufWinEnter", {
+  pattern = "*",
+  command = "silent! loadview",
+})
