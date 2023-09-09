8e60b2dfd0865fe3b9679c2cd5b381bd2ba5e3a0
ngoctd <tranngoc.contact@gmail.com>
2023-09-09 16:50:53 +0700

    update highlight

--- lua/custom/highlights.lua
+++ lua/custom/highlights.lua
@@ -12,10 +12,26 @@ M.override = {
   CursorLine = { bg = "#353342" },
   NormalFloat = { bg = "#181825" },
   ColorColumn = { bg = "#222235" },
+  TblineFill = { bg = "#181825" },
   Normal = { fg = "#c6c8d1", bg = "#181825" },
+
   NvimTreeNormal = { fg = "#c6c8d1", bg = "#1e1e2e" },
   NvimTreeNormalNC = { fg = "#c6c8d1", bg = "#1e1e2e" },
   NvimTreeWinSeparator = { fg = "#1e1e2e", bg = "#1e1e2e" },
+  -- StatusLine = { bg = "#ffffff" },
+
+  -- StatusLine = { bg = "#FFFFFF", fg = "#FFFFFF" },
+  -- StatusLineMode = { bg = "#FFFFFF", fg = "#FFFFFF" },
+  -- St_LspInfo = { bg = "#FFFFFF", fg = "#FFFFFF" },
+  -- St_TerminalMode = { bg = "#FFFFFF", fg = "#FFFFFF" },
+  -- StatusLineNC = { bg = "#181825" },
+  -- StatusLineMode = { fg = "#c6c8d1", bg = "#181825" },
+  -- St_NormalMode = { fg = "#c6c8d1", bg = "#181825" },
+  -- St_EmptySpace = { bg = "#181825" },
+  -- St_EmptySpace2 = { bg = "#181825" },
+  -- StatusLineMode = { fg = "#c6c8d1", bg = "#181825" },
+  -- StatusLine = { bg = "#181825" },
+  -- StatusLineNC = { bg = "#181825" },
 }
 
 ---@type HLTable
