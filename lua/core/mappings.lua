d032c2590de11b8efa93b785f6c58bdaeae87fd4
ngoctd <tranngoc.contact@gmail.com>
2023-09-28 08:32:18 +0700

    nav oldfile

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -29,6 +29,13 @@ M.general = {
     -- setting
     ["<leader>s"] = { function() end, "Set" },
     ["<leader>sn"] = { "<cmd>set nu! rnu!<cr>", "Toggle number" },
+    ["P"] = {
+      function()
+        vim.cmd "Telescope oldfiles"
+        -- vim.cmd.insert "<cr>"
+      end,
+      "Switch oldfiles",
+    },
 
     -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
     -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
