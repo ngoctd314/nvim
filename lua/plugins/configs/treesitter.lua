7ff70eed96ca9d5519834912565dec3ab957b313
ngoctd <tranngoc.contact@gmail.com>
2023-10-16 16:49:37 +0700

    disable hightlight when > 50KB

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,12 +1,19 @@
 local options = {
-  ensure_installed = { "lua" },
+    ensure_installed = { "lua" },
 
-  highlight = {
-    enable = true,
-    use_languagetree = true,
-  },
+    highlight = {
+        enable = true,
+        disable = function(lang, buf)
+            local max_filesize = 50 * 1024 -- 50 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
+        use_languagetree = true,
+    },
 
-  indent = { enable = true },
+    indent = { enable = true },
 }
 
 return options
