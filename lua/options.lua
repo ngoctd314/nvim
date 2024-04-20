be891a3835c67cb4b99060e547e16a95f9129ab7
ngoctd <tranngoc.contact@gmail.com>
2024-04-20 15:12:58 +0700

    add break line

--- lua/options.lua
+++ lua/options.lua
@@ -6,5 +6,9 @@ local opt = vim.opt
 opt.relativenumber = true
 opt.timeoutlen = 0
 
+-- wrap text at the end of the line
+vim.wo.wrap = true
+vim.wo.linebreak = true
+
 --opt.cursorline = true
 --opt.cursorlineopt = "both" -- to enable cursorline!
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,6 +6,7 @@ return {
       "stylua",
       "gopls",
       "jdtls",
+      "prettier",
     },
   },
 }
